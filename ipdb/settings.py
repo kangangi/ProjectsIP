6b0592fb138479c7aa88d39df9d5a1e53e975dac
kangangi <wanjikukangangi@gmail.com>
2020-06-05 22:05:14 +0300

    add admin

--- ipdb/settings.py
+++ ipdb/settings.py
@@ -131,3 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+cloudinary.config( 
+  cloud_name = config('CLOUD_NAME'),
+  api_key = config('CLOUDINARY_API_KEY'), 
+  api_secret = config('CLOUDINARY_API_SECRET'),
+)
