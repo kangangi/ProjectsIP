596e4a5f9bdf5c265ebe0ac2fdc5058282c6791d
kangangi <wanjikukangangi@gmail.com>
2020-06-07 18:54:50 +0300

    add media files

--- ipdb/settings.py
+++ ipdb/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -146,4 +147,7 @@ cloudinary.config(
   api_key = config('CLOUDINARY_API_KEY'), 
   api_secret = config('CLOUDINARY_API_SECRET'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name = "index"),
@@ -14,3 +16,6 @@ urlpatterns = [
     path('api/profiles/', views.ProfileList.as_view()),
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
