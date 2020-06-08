27c08a0697595a3c6ba5c91b757404c0f2ec6779
kangangi <wanjikukangangi@gmail.com>
2020-06-08 16:33:02 +0300

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ipdb.wsgi --log-file -
--- ipdb/settings.py
+++ ipdb/settings.py
@@ -14,6 +14,7 @@ import os
 from decouple import config,Csv
 import dj_database_url
 import cloudinary 
+import django_heroku
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
@@ -71,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ipdb.urls'
@@ -132,12 +134,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -148,6 +156,5 @@ cloudinary.config(
   api_secret = config('CLOUDINARY_API_SECRET'),
 )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
